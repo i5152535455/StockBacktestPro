ed2c2796a1fb65b323495158eb9a6776e3f440b6
i5152535455 <i5152535455@gmail.com>
2026-07-12 18:17:28 +0800

    v3.1 Triple Target logic completed

--- backtest_engine_ema60240.py
+++ backtest_engine_ema60240.py
@@ -44,9 +44,9 @@ def run_backtest(df, verbose=True):
 
     for _, row in df.iterrows():
 
-        # ===================
-        # 買進
-        # ===================
+    # ===================
+    # 買進
+    # ===================
         if row["BUY"] and not position:
 
             position = True
@@ -58,6 +58,45 @@ def run_backtest(df, verbose=True):
 
             partial_exit = False
 
+        # ===================
+        # 第一次獲利：漲三倍賣 1/3
+        # ===================
+        elif (
+            position
+            and (not partial_exit)
+            and row["Close"] >= buy_price * 3
+        ):
+
+            sell_price = row["Close"]
+            sell_date = row["Date"]
+
+            profit = calculate_profit(
+                buy_price,
+                sell_price
+            )
+
+            profit_amount = calculate_profit_amount(
+                config.POSITION_SIZE / 3,
+                buy_price,
+                sell_price
+            )
+
+            trades.append({
+
+                "Buy Date": buy_date,
+                "Buy Price": buy_price,
+                "Sell Date": sell_date,
+                "Sell Price": sell_price,
+                "Position": 1 / 3,
+                "Profit %": round(profit, 2),
+                "Profit Amount": round(profit_amount, 0),
+                "Exit Reason": "Triple Target"
+
+            })
+
+            position_size = 2 / 3
+
+            partial_exit = True
 
         # ===================
         # 跌破 EMA60 出場
@@ -70,42 +109,30 @@ def run_backtest(df, verbose=True):
             pf.sell(sell_date, sell_price)
 
             profit = calculate_profit(buy_price, sell_price)
- 
 
             profit_amount = calculate_profit_amount(
-            config.POSITION_SIZE,
-            buy_price,
-            sell_price
+                config.POSITION_SIZE * position_size,
+                buy_price,
+                sell_price
             )
 
             trades.append({
-            "Buy Date": buy_date,
-            "Buy Price": buy_price,
-            "Sell Date": sell_date,
-            "Sell Price": sell_price,
-            "Profit %": round(profit, 2),
-            "Profit Amount": round(profit_amount, 0),
-            "Exit Reason": "EMA60 Exit"
+
+                "Buy Date": buy_date,
+                "Buy Price": buy_price,
+                "Sell Date": sell_date,
+                "Sell Price": sell_price,
+                "Position": position_size,
+                "Profit %": round(profit, 2),
+                "Profit Amount": round(profit_amount, 0),
+                "Exit Reason": "EMA60 Exit"
+
             })
-           
 
             position = False
             position_size = 0
             buy_price = 0
             buy_date = None
-
-        # ===================
-        # 第一次獲利：漲三倍賣 1/3
-        # ===================
-        elif (
-             position
-             and (not partial_exit)
-             and row["Close"] >= buy_price * 3
-            ):
-
-         print(f"{buy_date} 三倍價達成，賣出 1/3")
-
-         partial_exit = True
     # 下一步會真正實作賣出1/3
  
 
@@ -122,7 +149,7 @@ def run_backtest(df, verbose=True):
         profit = calculate_profit(buy_price, sell_price)
 
         profit_amount = calculate_profit_amount(
-        config.POSITION_SIZE,
+        config.POSITION_SIZE * position_size,
         buy_price,
         sell_price
         )
@@ -132,6 +159,7 @@ def run_backtest(df, verbose=True):
         "Buy Price": buy_price,
         "Sell Date": sell_date,
         "Sell Price": sell_price,
+        "Position": position_size,
         "Profit %": round(profit, 2),
         "Profit Amount": round(profit_amount, 0),
         "Exit Reason": "End of Backtest"
